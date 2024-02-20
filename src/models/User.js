25116598ad9997485bcddc5e804647296b0547f1
bryansuarez2004 <bryansf2004@gmail.com>
2024-02-20 18:03:15 -0500

    agregando cambios

--- src/models/User.js
+++ src/models/User.js
@@ -26,13 +26,15 @@ const User = sequelize.define('user', {
   },
 });
 
+
+//esto eliminar el password para que no se vea cuando se retorne la respuesta en cada controlador
 User.prototype.toJSON = function () {
   const values = Object.assign({}, this.get());
   delete values.password;
   return values;
 }
 
-
+//esto es para encriptar la contrase;a cada vez que se crea un usuario, es un hook que intercepta antes de que se cree en la base de datos
 User.beforeCreate(async (user) => {
   const password = user.password
   const hashedPassword = await bcrypt.hash(password, 10)
