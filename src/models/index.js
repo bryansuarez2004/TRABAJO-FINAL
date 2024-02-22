a11c3b65cb6fa2a8ade853b3fdafe91144b5fcf5
bryansuarez2004 <bryansf2004@gmail.com>
2024-02-22 14:04:26 -0500

    tabla product terminada

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,9 @@
 const Category = require("./Category");
+const Product = require("./Product");
 const User = require("./User");
+
+
+// en la tabla product se estara generando la columna categoryID
+Product.belongsTo(Category)
+Category.hasMany(Product)
 
